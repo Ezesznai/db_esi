ed25e847a6a908d77bdb72b7b3ae71b5ad47f6b2
Ezesznai <108875180+Ezesznai@users.noreply.github.com>
2024-06-04 10:10:57 -0300

    Database sprint 1

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express")
+
+const app = express()
+
+app.listen(3000)
+
+console.log("server on port ${3000}")
+
+const cards = [
+  { id: 1, img: 'C:\Users\47653943\Desktop\backend\memotest\1' },
+  { id: 2, img: 'C:\Users\47653943\Desktop\backend\memotest\2' },
+  { id: 3, img: 'C:\Users\47653943\Desktop\backend\memotest\3' },
+/*  { id: 4, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 5, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 6, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 7, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 8, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 9, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 10, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 11, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 12, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 13, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 14, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 15, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  */
+  { id: 1, img: 'C:\Users\47653943\Desktop\backend\memotest\1' },
+  { id: 2, img: 'C:\Users\47653943\Desktop\backend\memotest\2' },
+  { id: 3, img: 'C:\Users\47653943\Desktop\backend\memotest\3' },
+ /* { id: 4, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 5, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 6, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 7, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 8, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 9, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 10, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 11, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 12, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 13, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 14, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 15, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  */
+];
+
+
+app.get('/cards', (req, res) => {
+  res.json(cards);
+});
+
