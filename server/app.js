f352481c1e487cb52e78601894a5ca5447bcd796
Ezesznai <108875180+Ezesznai@users.noreply.github.com>
2024-06-05 08:56:10 -0300

    trying app.js

--- server/app.js
+++ server/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express()
-const PORT = 8000
+//const PORT = 8000
 
 app.use(cors({
     origin: 'https://siaesi.vercel.app'
   }));
-
+/*
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
@@ -19,3 +19,47 @@ app.get('/about', (req, res) => {
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
 })
+*/
+console.log("server on port ${3000}")
+
+
+const cards = [
+  { id: 1, img: 'C:\Users\47653943\Desktop\backend\memotest\1' },
+  { id: 2, img: 'C:\Users\47653943\Desktop\backend\memotest\2' },
+  { id: 3, img: 'C:\Users\47653943\Desktop\backend\memotest\3' },
+/*  { id: 4, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 5, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 6, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 7, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 8, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 9, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 10, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 11, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 12, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 13, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 14, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 15, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  */
+  { id: 1, img: 'C:\Users\47653943\Desktop\backend\memotest\1' },
+  { id: 2, img: 'C:\Users\47653943\Desktop\backend\memotest\2' },
+  { id: 3, img: 'C:\Users\47653943\Desktop\backend\memotest\3' },
+ /* { id: 4, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 5, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 6, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 7, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 8, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 9, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 10, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 11, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 12, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 13, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 14, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  { id: 15, img: 'C:\Users\47653943\Desktop\backend\memotest' },
+  */
+];
+
+app.get('/cards', (req, res) => {
+  res.json(cards);
+});
+
+app.listen(3000)
