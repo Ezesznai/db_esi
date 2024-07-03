3acf916cdefbf09b97ee4892d6b0bdc9b512bfa1
Ezesznai <108875180+Ezesznai@users.noreply.github.com>
2024-07-03 08:50:19 -0300

    working on index cards

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const apiRoutes = require("./api/index.js")
 const app = express();
 app.use(express.json());
 app.use("/api", apiRoutes)
-
+/*
 app.get("/", (req, res)=> {
     res.json({message: "HOLISS"})
 })
@@ -64,7 +64,39 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
-
+*/
+// Datos iniciales
+const cards = [
+    { id: 1, img: 'C:/Users/47653943/Desktop/backend/memotest/1' },
+    { id: 2, img: 'C:/Users/47653943/Desktop/backend/memotest/2' },
+    { id: 3, img: 'C:/Users/47653943/Desktop/backend/memotest/3' },
+    { id: 1, img: 'C:/Users/47653943/Desktop/backend/memotest/1' },
+    { id: 2, img: 'C:/Users/47653943/Desktop/backend/memotest/2' },
+    { id: 3, img: 'C:/Users/47653943/Desktop/backend/memotest/3' },
+  ];
+  
+  // Guardar los datos en la base de datos
+  async function saveCards() {
+    for (const card of cards) {
+      await prisma.card.create({
+        data: {
+          img: card.img
+        }
+      });
+    }
+    console.log("Datos guardados en la base de datos");
+  }
+  
+  // Ruta para obtener los datos
+  app.get('/', async (req, res) => {
+    const allCards = await prisma.card.findMany();
+    res.json(allCards);
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running on port 3000`);
+    saveCards();
+  });
 // Manejo de errores y desconexión de Prisma
 process.on('SIGINT', async () => {
   await prisma.$disconnect();
