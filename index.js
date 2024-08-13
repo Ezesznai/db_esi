42c5e9c5a89b9bb16f9916498e74c210191c216c
Ezesznai <108875180+Ezesznai@users.noreply.github.com>
2024-08-13 10:34:02 -0300

    agregando codigo para pasar a json

--- index.js
+++ index.js
@@ -6,43 +6,8 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 app.use("/api", apiRoutes)
-/*
-app.get("/", (req, res)=> {
-    res.json({message: "HOLISS"})
-})
-
-// Escuchar en el puerto
-app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
-});
-*/
-// Datos iniciales
-// const cards = [
-//     { id: 1, img: 'https://res.cloudinary.com/dp2xcfg7x/image/upload/v1722344735/1_j4xwur.jpg' },
-//     { id: 2, img: 'memotest\2.jpg' },
-//     { id: 3, img: 'memotest\3.jpg' },
-//     { id: 1, img: 'memotest\1.jpg' },
-//     { id: 2, img: 'memotest\2.jpg' },
-//     { id: 3, img: 'memotest\3.jpg' },
-//   ];
   
-//   // Guardar los datos en la base de datos
-//   async function saveCards() {
-//     for (const card of cards) {
-//       await prisma.card.create({
-//         data: {
-//           img: card.img
-//         }
-//       });
-//     }
-//     console.log("Datos guardados en la base de datos");
-//   }
-  
-  
-  
-  
-  
-  // Ruta para obtener los datos
+  // Ruta para obtener los datos //necesito hacer un commit
   app.get('/', async (req, res) => {
     const allCards = await prisma.card.findMany();
     res.json(allCards);
