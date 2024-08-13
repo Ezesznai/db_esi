b21ea83fba3b0d5d3f55e97d0a6a264312c53993
Ezesznai <108875180+Ezesznai@users.noreply.github.com>
2024-08-13 10:37:40 -0300

    volviendo un commit atras

--- api/index.js
+++ api/index.js
@@ -20,45 +20,7 @@ app.post('/', async (req, res) => {
 export default async (req, res) => {
   if (req.method === 'GET') {
     try {
-      import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export default async (req, res) => {
-  const {
-    query: { id },
-    method,
-  } = req;
-
-  if (method === 'GET') {
-    try {
-      const puzzleWord = await prisma.puzzleWord.findUnique({
-        where: { id: parseInt(id, 10) },
-        select: {
-          word: true,
-        },
-      });
-
-      if (puzzleWord) {
-        res.status(200).json(puzzleWord);
-      } else {
-        res.status(404).json({ message: `PuzzleWord with id ${id} not found` });
-      }
-    } catch (error) {
-      console.error('Error retrieving puzzle word:', error);
-      res.status(500).json({ error: 'Error retrieving puzzle word' });
-    }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${method} Not Allowed`);
-  }
-};
-
-
-
-
-
-     /* // Recuperar todas las palabras de la tabla PuzzleWord
+      // Recuperar todas las palabras de la tabla PuzzleWord
       const words = await prisma.puzzleWord.findMany({
         select: {
           id: true,
@@ -79,5 +41,3 @@ export default async (req, res) => {
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 };
-
-*/
