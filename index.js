f21a2916a2482408482cbbda52dccee74b1c75d1
Ezesznai <108875180+Ezesznai@users.noreply.github.com>
2024-12-04 11:27:55 -0300

    sacando /

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ app.use("/api", apiRoutes)
 app.get('/favicon.ico', (req, res) => res.status(204));
 
   
-  // Ruta para obtener los datos //necesito hacer un commit
+/*
   app.get('/', async (req, res) => {
     const allCards = await prisma.card.findMany();
     res.json(allCards);
   });
-  
+  */
 app.listen(port, () => {
   console.log(`Server running on port 3000`);
   // saveCards();
 });
-// Manejo de errores y desconexión de Prisma
+
 process.on('SIGINT', async () => {
   await prisma.$disconnect();
   process.exit();
