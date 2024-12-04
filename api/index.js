81ae78ec6c6c6da08662f6e9a3a2efa7d80b7a87
Ezesznai <4765943@est.ort.edu.ar>
2024-12-04 09:47:40 -0300

    saque codigo de abajo

--- api/index.js
+++ api/index.js
@@ -173,30 +173,31 @@ app.get("/usuarios", async (req, res) => {
 
 // Ruta para gestionar PuzzleWords
 export default app
- async (req, res) => {
-   if (req.method === 'GET') {
-     try {
-       // Recuperar todas las palabras de la tabla PuzzleWord
-       const words = await prisma.puzzleWord.findMany({
-         select: {
-           id: true,
-           word: true,          
-         },
-       });
+
+ //async (req, res) => {
+ //  if (req.method === 'GET') {
+  //   try {
+    //   // Recuperar todas las palabras de la tabla PuzzleWord
+      // const words = await prisma.puzzleWord.findMany({
+        // select: {
+          // id: true,
+         //  word: true,          
+        // },
+      // });
 
        // Enviar la respuesta con las palabras
-       res.status(200).json(words);
-     } catch (error) {
+    //   res.status(200).json(words);
+    // } catch (error) {
        // Manejo de errores
-       console.error('Error al recuperar las palabras:', error);
-       res.status(500).json({ error: 'Error al recuperar las palabras' });
-     }
-   } else {
+     //  console.error('Error al recuperar las palabras:', error);
+      // res.status(500).json({ error: 'Error al recuperar las palabras' });
+   //  }
+  // } else {
      // Manejo de métodos no permitidos
-     res.setHeader('Allow', ['GET']);
-     res.status(405).end(`Method ${req.method} Not Allowed`);
-   }
- };
+    // res.setHeader('Allow', ['GET']);
+    // res.status(405).end(`Method ${req.method} Not Allowed`);
+  // }
+// };
 
 
 
